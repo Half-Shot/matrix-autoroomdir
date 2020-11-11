f5c8961a1d167f5b6ef7330f593dc57f2b5360dc
Will Hunt <willh@matrix.org>
2020-11-11 10:42:09 +0000

    Check power levels

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const axios = require('axios');
+const ROOM_PARTS = require('./roomparts.json');
 const BRIDGE_ACCESS_TOKEN = process.env.BRIDGE_ACCESS_TOKEN;
 const USER_ACCESS_TOKEN = process.env.USER_ACCESS_TOKEN;
-const ROOM_PARTS = [
-]
 
 const bridgeClient = axios.create({
     baseURL: process.env.BRIDGE_URL,
@@ -19,9 +18,19 @@ const userClient = axios.create({
 });
 
 async function main() {
-    const whoamiBridge = await bridgeClient.get("/_matrix/client/r0/account/whoami");
-    const whoamiUser = await userClient.get("/_matrix/client/r0/account/whoami");
+    // Check auth
+    const whoamiBridge = (await bridgeClient.get("/_matrix/client/r0/account/whoami")).data;
+    const whoamiUser = (await userClient.get("/_matrix/client/r0/account/whoami")).data;
     console.log(whoamiBridge, whoamiUser);
+
+    // Run cycle
+    for (const iterator of ROOM_PARTS) {
+        const alias = `#xmpp_${iterator.replace('@', '_')}:matrix.org`;
+        const [ room_id ] = (await bridgeClient.get(`/_matrix/client/r0/join/${encodeURIComponent(alias)}`)).data;
+        const powerLevels = (await bridgeClient.get(`/_matrix/client/r0/rooms/${encodeURIComponent(room_id)}/state/m.room.power_levels/`)).data;
+        console.log(alias);
+        console.log(powerLevels);
+    }
 }
 
 main().then(() => {
