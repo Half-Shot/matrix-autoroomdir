6067133439b6eaaf6f23de48756a2c256d77a6e9
Will Hunt <willh@matrix.org>
2020-11-11 10:34:13 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const BRIDGE_ACCESS_TOKEN = process.env.BRIDGE_ACCESS_TOKEN;
+const USER_ACCESS_TOKEN = process.env.USER_ACCESS_TOKEN;
+const ROOM_PARTS = [
+]
+
+const bridgeClient = axios.create({
+    baseURL: process.env.BRIDGE_URL,
+    headers: {
+        'Authentication': `Bearer ${BRIDGE_ACCESS_TOKEN}`
+    }
+});
+
+const userClient = axios.create({
+    baseURL: process.env.CLIENT_URL,
+    headers: {
+        'Authentication': `Bearer ${USER_ACCESS_TOKEN}`
+    }
+});
+
+async function main() {
+    const whoamiBridge = await bridgeClient.get("/_matrix/client/r0/account/whoami");
+    const whoamiUser = await userClient.get("/_matrix/client/r0/account/whoami");
+    console.log(whoamiBridge, whoamiUser);
+}
+
+main().then(() => {
+    console.log("Script finished");
+}).catch((e) => {
+    console.error("Script failed:", e);
+})
